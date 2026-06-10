088ef3de2d40562924f8a78498b930a8a5337ad5
samridhipandita-source <samridhipandita@gmail.com>
2026-06-10 15:56:44 +0000

    text formalised

--- basic.c
+++ basic.c
@@ -224,7 +224,7 @@ int main()
 
     while(1)
     {
-    printf("choose among the following:\n1.point\n2.rectangle\n3.line\n4.triangle\n5.circle\n6.display canvas\n7.delete area\n8.clear canvas\n9.modify area\n10.exit canvas\n");
+    printf("Choose among the following:\n1.Point\n2.Rectangle\n3.Line\n4.Triangle\n5.Circle\n6.Display canvas\n7.Delete area\n8.Clear canvas\n9.Modify area\n10.Exit canvas\n");
     scanf("%d", &choice);
     
     if(choice==1)
@@ -238,7 +238,7 @@ int main()
 
         if(x<0||y<0||x>=width||y>=height)
         {
-            printf("invalid point coordinates\n");
+            printf("Invalid point coordinates.\n");
             continue;
         }
         else
@@ -254,20 +254,20 @@ int main()
         printf("Rectangle X&Y coordinates: ");
         scanf("%d %d", &rectX, &rectY);
 
-        printf("Rectangle width and height: ");
+        printf("Rectangle Width and Height: ");
         scanf("%d %d", &rectWidth, &rectHeight);
 
         if(rectX<0||rectY<0||rectWidth<=0||rectHeight<=0||rectX+rectWidth>width||rectY+rectHeight>height)
         {
-            printf("invalid rectangle coordinates\n");
+            printf("Invalid rectangle coordinates.\n");
             continue;
         }
         else
         {
-            printf("1. hollow Rectangle\n");
-            printf("2. filled Rectangle\n");
+            printf("1. Hollow Rectangle\n");
+            printf("2. Filled Rectangle\n");
         
-            printf("input 1 for hollow rect, 2 for filled rect \n");
+            printf("Input 1 for hollow rectangle, 2 for filled rectangle \n");
             scanf("%d", &res);
 
             drawRectangle(rectX,rectY,rectWidth,rectHeight,res,height,width,canvas);
@@ -277,13 +277,13 @@ int main()
     else if(choice==3)
     {
         int x1,y1,x2,y2;
-        printf("enter point1 x&y coordinates:");
+        printf("Enter point1 x&y coordinates:");
         scanf("%d %d", &x1, &y1);
-        printf("enter point2 x&y coordinates:");
+        printf("Enter point2 x&y coordinates:");
         scanf("%d %d", &x2, &y2);
         if(x1<0||x1>=width||y1<0||y1>=height||x2<0||x2>=width||y2<0||y2>=height)
         {
-            printf("invalid line coordinates\n");
+            printf("Invalid line coordinates.\n");
             continue;
         }
         else
@@ -295,16 +295,16 @@ int main()
     else if(choice==4)
     {
         int x1,x2,x3,y1,y2,y3;
-        printf("enter coordinates for vertex1:");
+        printf("Enter coordinates for vertex1:");
         scanf("%d %d", &x1, &y1);
-        printf("enter coordinates for vertex2:");
+        printf("Enter coordinates for vertex2:");
         scanf("%d %d", &x2, &y2);
-        printf("enter coordinates for vertex3:");
+        printf("Enter coordinates for vertex3:");
         scanf("%d %d", &x3, &y3);
 
         if(x1<0||x1>=width||y1<0||y1>=height||x2<0||x2>=width||y2<0||y2>=height||x3<0||x3>=width||y3<0||y3>=height)
         {
-            printf("invalid triangle coordinates\n");
+            printf("Invalid triangle coordinates.\n");
             continue;
         }
         else
@@ -316,14 +316,14 @@ int main()
     else if(choice==5)
     {
         int centerX, centerY, radius;
-        printf("enter center x&y coordinates for circle:");
+        printf("Enter center x&y coordinates for circle:");
         scanf("%d %d", &centerX, &centerY);
-        printf("enter radius of circle:");
+        printf("Enter radius of circle:");
         scanf("%d", &radius);
 
         if(centerX-radius<0||centerY-radius<0||centerX+radius>width-1||centerY+radius>height-1||radius<=0)
         {
-            printf("invalid circle parameters\n");
+            printf("Invalid circle parameters.\n");
             continue;
         }
         
@@ -344,14 +344,14 @@ int main()
     {
         int deleteX, deleteY, deleteWidth, deleteHeight;
 
-        printf("enter x&y coordinates of area to delete: ");
+        printf("Enter x&y coordinates of area to delete: ");
         scanf("%d %d", &deleteX, &deleteY);
-        printf("enter height & width of area to delete: ");
+        printf("Enter height & width of area to delete: ");
         scanf("%d %d", &deleteWidth, &deleteHeight);
 
         if(deleteX<0||deleteY<0||deleteWidth<=0||deleteHeight<=0||deleteX+deleteWidth>width||deleteY+deleteHeight>height)
         {
-            printf("delete area out of bounds\n");
+            printf("Delete area out of bounds.\n");
             continue;
         }
         else
@@ -371,18 +371,18 @@ int main()
         int areaWidth, areaHeight;
         int newX, newY;
 
-        printf("current area x&y coordinates: ");
+        printf("Current area x&y coordinates: ");
         scanf("%d %d", &oldX, &oldY);
 
-        printf("area width & height: ");
+        printf("Area width & height: ");
         scanf("%d %d", &areaWidth, &areaHeight);
 
-        printf("new area x&y coordinates: ");
+        printf("New area x&y coordinates: ");
         scanf("%d %d", &newX, &newY);
 
         if(oldX<0||oldY<0||oldX+areaWidth>width||oldY+areaHeight>height||newX<0||newY<0||newX+areaWidth>width||newY+areaHeight>height||areaWidth<=0||areaHeight<=0)
         {
-            printf("modification area out of bounds\n");
+            printf("Modification area out of bounds.\n");
             continue;
         }
 
@@ -391,12 +391,12 @@ int main()
     }
     else if(choice==10)
     {
-        printf("exiting canvas\n");
+        printf("Exiting canvas.\n");
         exit(0);
     }
     else
     {
-        printf("invalid choice, please try again.\n");
+        printf("Invalid choice, please try again.\n");
     }
 }
 return 0;
